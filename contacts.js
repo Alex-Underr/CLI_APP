e19046c5dcfd421d2812443ee6e2566e70674eef
Alex-underr <4ubs.pl@gmail.com>
2023-02-10 17:56:35 +0200

    Update contacts.js

---
+++ contacts.js
@@ -0,0 +1,32 @@
+// const contacts = require "./db"
+const fs = require("fs").promises;
+const path = require("path");
+// fs.readFile(filename, [options]) - читання файлу
+// fs.writeFile(filename, data, [options]) - запис файлу
+// fs.appendFile(filename, data, [options])- додавання у файл
+// fs.rename(oldPath, newPath) - перейменування файлу.
+// fs.unlink(path, callback) - видалення файлу.
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports;
